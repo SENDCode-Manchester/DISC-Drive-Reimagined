ccd5c645cb69c78a661c17d8756c57586267ff2c
PixelBedrock <113065795+PixelBedrock@users.noreply.github.com>
2025-02-12 10:42:45 +0000

    fix[backend]: change gemini prompt to ignore empty students/questions

--- packages/backend/src/geminiConfig.ts
+++ packages/backend/src/geminiConfig.ts
@@ -43,6 +43,8 @@ The input you will be given will be in JSON format, with "students" as an array 
 You must consider the student's age when rewriting the question to decide how complex of words you will use.
 Your response must be in British English so, for example, "color" becomes "colour" and "candies" becomes "sweets".
 If you're going to change the name of a person in the question, you must not directly use the student's name.
+If the student has empty fields (name is empty, age is 0 or interests is empty), you must ignore that student.
+If a question is empty, you must ignore that question.
 
 For example, if the question was "You have a bag containing 5 red, 3 green and 2 blue marbles. If you randomly draw two marbles one after the other without replacement, what is the probability that both marbles drawn are green?" and the interest was "Monsters, Inc." the response would be:
 "Mike Wazowski has a bag containing 5 red scream canisters, 3 green laugh canisters and 2 blue energy canisters. If you randomly draw 2 canisters one after the other without replacement, what is the probability that both canisters drawn are green laugh canisters?"`
